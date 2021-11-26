98ecd630c83a11d7212efea590f8383f48926d57
Maxim <74974283+maximka76667@users.noreply.github.com>
2021-11-26 09:37:47 +0100

    ej3

---
+++ src/print_entre_N_y_K_ej03/Main03.java
@@ -0,0 +1,26 @@
+package print_entre_N_y_K_ej03;
+
+import java.util.Scanner;
+
+public class Main03 {
+	
+	public static void printEntre(int N, int K) {
+		for(int i = N; i <= K; i++) System.out.print(i + " ");
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.print("Número N: ");
+		int N = entrada.nextInt();
+		
+		System.out.print("Número K: ");
+		int K = entrada.nextInt();
+
+		printEntre(N, K);
+
+		entrada.close();
+	}
+
+}
