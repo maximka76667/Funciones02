f9294e31ea42c5d7d57aca50a32766401b8a2e99
Maxim <74974283+maximka76667@users.noreply.github.com>
2021-11-26 09:20:09 +0100

    Initial commit

---
+++ src/print_N_times_ej01/Main01.java
@@ -0,0 +1,22 @@
+package print_N_times_ej01;
+
+import java.util.Scanner;
+
+public class Main01 {
+	
+	public static void printN(int N, String mensaje) {
+		for(int i = 0; i < N; i++) System.out.println(mensaje);
+	}
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.print("Número: ");
+		int N = entrada.nextInt();
+
+		printN(N, "Módulo ejecutándose");
+
+		entrada.close();
+	}
+
+}
