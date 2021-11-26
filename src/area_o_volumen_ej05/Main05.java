a41635513a2cf6458924b8ce5e68c2c301d98af9
Maxim <74974283+maximka76667@users.noreply.github.com>
2021-11-26 09:55:17 +0100

    ej5

---
+++ src/area_o_volumen_ej05/Main05.java
@@ -0,0 +1,49 @@
+package area_o_volumen_ej05;
+
+import java.util.Scanner;
+
+public class Main05 {
+	
+	public static double calcularArea(double radio) {
+		return Math.PI * Math.pow(radio, 2);
+	}
+	
+	public static double calcularVolumen(double radio, double altura) {
+		return Math.PI * Math.pow(radio, 2) * altura;
+	}
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("Calcular area o volumen (a/v): ");
+		char tipo = entrada.nextLine().charAt(0);
+		
+		double radio;
+		String resultado;
+
+		switch (tipo) {
+		case 'a':
+			System.out.print("Radio: ");
+			radio = entrada.nextDouble();
+			resultado = Double.toString(calcularArea(radio));
+			break;
+			
+		case 'v':
+			System.out.print("Radio: ");
+			radio = entrada.nextDouble();
+			System.out.print("Altura: ");
+			double altura = entrada.nextDouble();
+			resultado = Double.toString(calcularVolumen(radio, altura));
+			break;
+
+		default:
+			resultado = "Error";
+			break;
+		}
+
+		System.out.println("Resultado: " + resultado);
+
+		entrada.close();
+	}
+
+}
