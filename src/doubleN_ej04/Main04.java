6f3a1daa178b1d03ba6deafeeb4344f972840266
Maxim <74974283+maximka76667@users.noreply.github.com>
2021-11-26 09:41:03 +0100

    ej4

---
+++ src/doubleN_ej04/Main04.java
@@ -0,0 +1,22 @@
+package doubleN_ej04;
+
+import java.util.Scanner;
+
+public class Main04 {
+	
+	public static double doubleN(double N) {
+		return N * 2;
+	}
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.print("Número: ");
+		double N = entrada.nextDouble();
+
+		System.out.println("Número doble: " + doubleN(N));
+
+		entrada.close();
+	}
+
+}
